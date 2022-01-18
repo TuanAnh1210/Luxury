5e7aafa3f434e209a1eff8d33ec9d4033af7556a
Tony Nguyen <tuananh1210085213@gmail.com>
2022-01-18 12:01:12 +0700

    secondCommit

--- assets/css/page.js
+++ assets/css/page.js
@@ -9,17 +9,17 @@ const headerNav = A('.header__navbar');
 
 
 
-A('.header__user').onclick = function() {
+A('.header__user').onclick = () => {
     modal1.classList.add('open');
     authForm2.classList.add('open')
 }
 
-A('.auth-form--register.active').onclick = function() {
+A('.auth-form--register.active').onclick = function () {
     authForm2.classList.remove('open');
     authForm1.classList.add('open')
 }
 
-A('.js-login').onclick = function() {
+A('.js-login').onclick = () => {
     authForm1.classList.remove('open');
     authForm2.classList.add('open')
 }
@@ -27,11 +27,11 @@ A('.js-login').onclick = function() {
 const iconMobile = AA('.mobile-icon');
 
 for (a = 0; a < iconMobile.length; a++) {
-    iconMobile[1].onclick = function() {
+    iconMobile[1].onclick = function () {
         modal1.classList.add('open');
         authForm2.classList.add('open')
     }
-    iconMobile[0].onclick = function() {
+    iconMobile[0].onclick = function () {
         if (headerNav == A('.header__navbar.open')) {
             headerNav.classList.remove('open')
         }
@@ -44,7 +44,7 @@ for (a = 0; a < iconMobile.length; a++) {
 
 const modalOverlay = document.querySelector('.modal__overlay');
 
-modalOverlay.onclick = function() {
+modalOverlay.onclick = function () {
     authForm1.classList.remove('open');
     authForm2.classList.remove('open');
     modal1.classList.remove('open');
@@ -56,27 +56,27 @@ modalOverlay.onclick = function() {
 
 // mobile
 
-$(document).ready(function(){
+$(document).ready(function () {
     $(".owl-carousel").owlCarousel();
 });
 
 $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        autoplay:true,
-        autoplayTimeout:3000,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:1
-            },
-            1000:{
-                items:1
-            }
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
         }
+    }
 })
 
 const navbarMobile = A('.sub-header-list--mobile');
@@ -84,7 +84,7 @@ const navbarMobile = A('.sub-header-list--mobile');
 const headerNavList = AA('.js-listMobile');
 
 for (Nav = 0; Nav < headerNavList.length; Nav++) {
-    headerNavList[Nav].onclick = function() {
+    headerNavList[Nav].onclick = function () {
         navbarMobile.classList.toggle('open')
     }
 }
