07117c15e0297fead8c5c2f38254e1a709720645
TuanAnhNguyen <tuananh1210085213@gmail.com>
2022-04-14 15:31:08 +0700

    commit

--- assets/css/page.js
+++ assets/css/page.js
@@ -6,6 +6,95 @@ const authForm2 = A('.auth-form2');
 const authForm1 = A('.auth-form1');
 const headerNav = A('.header__navbar');
 
+// GOtoTop
+
+const goToTop = A('.goToTop')
+
+
+const handleScrooll = () => {
+    if (window.scrollY >= 800) {
+        goToTop.style.display = "flex"
+    }
+    else {
+        goToTop.style.display = "none"
+
+    }
+}
+
+goToTop.onclick = () => {
+    window.location.href = '#'
+}
+
+
+
+window.addEventListener('scroll', handleScrooll)
+
+// end GotoTop
+
+
+// login
+const account = {
+    email: 'tuananh0852131210@gmail.com',
+    password: 'Tuananh12'
+}
+
+let userName = A('#userName')
+let passWord = A('#passWord')
+const login = AA('.auth-form-btn')
+
+function validateUser() {
+    if (userName.value == account.email) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+function validatePassWord() {
+    if (passWord.value == account.password) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+userName.onkeyup = (e) => {
+    if (e.keyCode === 13) {
+        alert('Please fill it out completely!')
+    }
+}
+
+passWord.onkeyup = (e) => {
+    if (e.keyCode === 13) {
+        const isUser = validateUser()
+        const isPassWord = validatePassWord()
+        if (isPassWord && isUser) {
+            window.location.href = 'https://github.com/TuanAnh1210'
+
+        }
+        else {
+            alert('Please enter the correct information !')
+
+        }
+    }
+
+}
+
+
+login[0].onclick = () => {
+    const isUser = validateUser()
+    const isPassWord = validatePassWord()
+    if (isUser && isPassWord) {
+        window.location.href = 'https://github.com/TuanAnh1210'
+    }
+    else {
+        alert('Please enter the correct information !')
+    }
+}
+
+
+// end login
 
 
 
